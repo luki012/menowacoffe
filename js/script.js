1600644e3037f5381b5077aef39be0faf4a6c6a9
luki irwanto <lukiluki6134@gmail.com>
2023-12-28 11:19:24 +0700

    first

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// tools toogle utk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+const hMenu = document.querySelector('#hambuerger-menu')
+
+//ketika hamburgermenu di click
+hMenu.addEventListener("click", function(){
+    navbarNav.classList.toggle('active')
+})
+//clik diluar untuk menghilangkan nav
+document.addEventListener("click",function(a){
+    if(!hMenu.contains(a.target)&& !navbarNav.contains(a.target)){
+        navbarNav.classList.remove('active')
+    }
+})
+
+//ketika search di click
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchBtn = document.querySelector("#search-button");
+
+searchBtn.addEventListener("click", function(){
+    searchForm.classList.toggle("active");
+    searchBox.focus();
+})
+
+//click diluar untuk menghilangkan search
+document.addEventListener("click", function(a){
+    if(!searchBtn.contains(a.target)&& !searchForm.contains(a.target)){
+        searchForm.classList.remove("active")
+    }
+})
+
+
+//tangkap shopping card
+
+const shopBtn = document.querySelector("#shopping-card-button");
+const shopCard = document.querySelector(".shoping-card");
+
+//ketika shoping-card-btn di click
+shopBtn.onclick = (e) => {
+    shopCard.classList.toggle("active");
+    e.preventDefault()
+};
+
+//menghilangkan selain shopping card button
+
+document.addEventListener("click", function(a){
+    if(!shopBtn.contains(a.target)&& !shopCard.contains(a.target)){
+        shopCard.classList.remove("active")
+    }
+})
+
+//tangkap element modal
+const modal = document.querySelector("#modal-item-detail");
+const eyePr = document.querySelectorAll(".eye-product");
+
+//ketika eye di click
+
+eyePr.forEach((btn)=>{
+    btn.onclick = (e) => {
+        modal.style.display = "flex";
+        e.preventDefault()
+    } 
+})
+
+//click tombol close
+
+const tClose = document.querySelector(".close-icon");
+tClose.onclick = (e) => {
+    modal.style.display = "none";
+    e.preventDefault()
+}
+
+//click menghilangkan clik dmana saja
+
+window.onclick = (e) => {
+    if(e.target === modal){
+        modal.style.display ="none";
+    }
+}
---
+++ src/app.js
@@ -0,0 +1,114 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('prodact', () => ({
+        items: [
+            { id: 1, name: "Long-black", img: '1.jpg', price: 11000 },
+            { id: 2, name: "Capuchiono", img: '2.jpg', price: 13000 },
+            { id: 3, name: "Mocca", img: '3.jpg', price: 15000 },
+            { id: 4, name: "Coffe-Milk", img: '4.jpg', price: 20000 },
+        ],
+
+    }));
+
+    Alpine.store('card', {
+        item: [],
+        total: 0,
+        quantity: 0,
+        add(newBarang) {
+            //cek ada barang yag sama tidak di card
+            const cardItem = this.item.find((item) => item.id === newBarang.id)
+
+            //jika belum ada / card masih kosong
+            if (!cardItem) {
+                this.item.push({ ...newBarang, quantity: 1, total: newBarang.price });
+                this.quantity++;
+                this.total += newBarang.price;
+
+            } else {
+                //jika barang sudah ada, cek apakah barang beda atau sama dengan di card
+                this.item = this.item.map((item) => {
+                    //jika barang berbeda
+                    if (item.id !== newBarang.id) {
+                        return item;
+                    } else {
+                        //jika barang sudah ada, tambah quantiti dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+
+        },
+        remove(id) {
+            //ambil item yang di remove id nya
+            const cartItem = this.item.find((item) => item.id === id);
+
+            //Jika item lebih dari 1
+            if (cartItem.quantity > 1) {
+                //telusuri 1 1
+                this.item = this.item.map((item) => {
+                    //jika bukan barang yang di clik
+                    if (item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                //jika barangnya sisa 1
+                this.item = this.item.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+
+        }
+    });
+
+});
+
+
+
+//Form Validation
+const btnChekout = document.querySelector(".chekout-btn");
+btnChekout.disable = true;
+const form = document.getElementById('chekoutForm');
+
+form.addEventListener("keyup", () => {
+    for (let x = 0; x < form.elements.length; x++) {
+        if (form.elements[x].length !== 0) {
+            btnChekout.classList.remove('disable');
+            btnChekout.classList.add('disable');
+        } else {
+                 return false;
+            };
+        };
+          btnChekout.disable = false;
+          btnChekout.classList.remove('disable');
+    });
+
+
+//kirim data ketika tombol di click
+btnChekout.addEventListener('click', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    //const objData = new Object.fromEntries(data);
+    //console.log(objData);
+})
+
+
+//konversi rupiah
+
+const rp = (num) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(num);
+};
